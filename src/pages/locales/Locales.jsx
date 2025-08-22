15b81ed9d22596e38d1c7d0ac0b84afd20accc43
MichaelSilva.git <losoa1999@gmail.com>
2025-08-22 00:58:43 -0500

    pago y locales

--- src/pages/locales/Locales.jsx
+++ src/pages/locales/Locales.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
-
+import './Locales.css'
 const Locales = () => {
-  return (
-    <div>Locales</div>
-  )
+return (
+  <>
+    <h1 style={{ textAlign: "center", fontSize: "250%" }}>
+      calle Teodoro Valcarcel 183 Galería El Edén, Santiago de Surco 15023
+    </h1>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31206.608191126998!2d-77.01730802929148!3d-12.124053327704909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7d2e23d00b3%3A0x1133bbcdfb76ec90!2sServitec%20Total!5e0!3m2!1ses-419!2spe!4v1739769654072!5m2!1ses-419!2spe"
+      width="600"
+      height="450"
+      style={{ border: 0, width: "100%" }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+      title="Mapa Servitec Total"
+    ></iframe>
+    <img
+      src="https://effervescent-concha-1ab1e8.netlify.app/img/cede%20servitec.jpg"
+      style={{ width: "100%", height: "600px", objectFit: "fill" }}
+      alt="Cede Servitec"
+    />
+    <hr
+      style={{
+        width: "1100px",
+        backgroundColor: "black",
+        height: "4px",
+        marginTop: "80px",
+      }}
+    />
+    <h1 style={{ textAlign: "center", fontSize: "250%" }}>
+      Jr. Camaná 1178, Lima 15001
+    </h1>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.826534011238!2d-77.04020732517465!3d-12.055453388182054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c95c52ec6209%3A0x9f690f7b812a5006!2sWilson%20centro%20c%C3%B3mputo%20compras!5e0!3m2!1ses-419!2spe!4v1739771003542!5m2!1ses-419!2spe"
+      width="600"
+      height="450"
+      style={{ border: 0, width: "100%" }}
+      allowFullScreen=""
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+      title="Mapa Wilson Centro Computo"
+    ></iframe>
+    <img
+      src="https://effervescent-concha-1ab1e8.netlify.app/img/images.jpeg"
+      style={{ width: "100%", height: "600px", objectFit: "fill" }}
+      alt="Wilson Centro Computo"
+    />
+  </>
+)
 }
 
 export default Locales
--- src/pages/pago/Pago.jsx
+++ src/pages/pago/Pago.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
-
+import './Pago.css'
 const Pago = () => {
   return (
-    <div>Pago</div>
+    <>
+      <h1>MODALIDADES DE PAGO</h1>
+      <hr />
+      <div className="section">
+        <div className="img-pago">
+          <img src="https://effervescent-concha-1ab1e8.netlify.app/img/PLIN%202.png" alt="plin" />
+        </div>
+        <div className="dato">
+          <p>
+            PLIN: <br />
+            Cuenta Plin: 927-538-7421
+          </p>
+        </div>
+      </div>
+      <hr />
+      <div className="section">
+        <div className="img-pago">
+          <img src="https://effervescent-concha-1ab1e8.netlify.app/img/YAPE.png" alt="plin" />
+        </div>
+        <div className="dato">
+          <p>
+            YAPE: <br />
+            Cuenta yape: 987-452-613
+          </p>
+        </div>
+      </div>
+      <hr />
+      <div className="section">
+        <div className="img-pago">
+          <img src="https://effervescent-concha-1ab1e8.netlify.app/img/BCP.png" alt="banco" />
+        </div>
+        <div className="dato dato-banco">
+          <p>
+            Cuenta en Soles (PEN) - BCP <br />
+            CCI: 002-191-30456789231-10 <br />
+            Número de Cuenta: <br /> 191-3045678923
+          </p>
+        </div>
+        <div className="dato dato-banco">
+          <p>
+            Cuenta en Dólares (USD) - BCP <br />
+            CCI: 002-191-30456789231-10 <br />
+            Número de Cuenta: <br /> 191-3045678923
+          </p>
+        </div>
+      </div>
+      <hr />
+      <div className="section">
+        <div className="img-pago">
+          <img src="https://effervescent-concha-1ab1e8.netlify.app/img/BBVA.png" alt="banco" />
+        </div>
+        <div className="dato dato-banco">
+          <p>
+            Cuenta en Soles (PEN) - BBVA <br />
+            CCI: 002-191-30456789231-10 <br />
+            Número de Cuenta: <br /> 191-3045678923
+          </p>
+        </div>
+        <div className="dato dato-banco">
+          <p>
+            Cuenta en Dólares (USD) - BBVA <br />
+            CCI: 002-191-30456789231-10 <br />
+            Número de Cuenta: <br /> 191-3045678923
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
