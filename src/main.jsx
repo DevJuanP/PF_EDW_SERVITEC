280abbac70f69650dbbe3a832d86bc4aa914148f
JuanP <juanpajueloch@gmail.com>
2025-08-22 00:28:06 -0500

    rutas y locales(vacio)

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import Home from './pages/home/Home.jsx'
 import Sevicios from './pages/servicios/Servicios.jsx'
 import Productos from './pages/productos/Productos.jsx'
 import Delivery from './pages/delivery/Delivery.jsx'
+import Locales from './pages/Locales/Locales.jsx'
+import Negocio from './pages/negocio/Negocio.jsx'
+import Pago from './pages/pago/Pago.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,8 +18,11 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} >
           <Route index element={<Home/>} />
-          <Route path="/servicios" element={<Sevicios/>} />
           <Route path="/delivery" element={<Delivery/>} />
+          <Route path="/locales" element={<Locales/>} />
+          <Route path="/negocio" element={<Negocio/>} />
+          <Route path="/pago" element={<Pago/>} />
+          <Route path="/servicios" element={<Sevicios/>} />
           <Route path="/productos/:categoria" element={<Productos/>} />
         </Route>
       </Routes>    
