b09a40d5bfc6f28bac4d8e8ac63e8e79c7070286
GalacticLang <jhedan0101@gmail.com>
2025-08-22 11:18:50 -0500

    avanzando jhonatan

--- src/pages/negocio/Negocio.jsx
+++ src/pages/negocio/Negocio.jsx
@@ -2,7 +2,77 @@ import React from 'react'
 
 const Negocio = () => {
   return (
-    <div>Negocio</div>
+    <div className="modelo-container">
+      <h1>MODELO DE NEGOCIO</h1>
+      <p>
+        Nuestro modelo de negocio se basa en ofrecer productos de alta calidad y servicios excepcionales a nuestros clientes. Nos enfocamos en la venta de computadoras, laptops y componentes, así como en brindar servicios de reparación y mantenimiento técnico.
+      </p>
+      
+      <p>
+        Además, contamos con un equipo de profesionales capacitados para asesorar a nuestros clientes en la elección de los productos que mejor se adapten a sus necesidades. Nuestro objetivo es garantizar la satisfacción del cliente a través de una experiencia de compra fluida y un servicio postventa confiable.
+      </p>
+      <p>
+        En ServiTec, nos comprometemos a mantenernos actualizados con las últimas tendencias tecnológicas y a ofrecer productos innovadores que cumplan con los estándares más altos de calidad. Nuestro modelo de negocio se basa en la confianza, la transparencia y el compromiso con nuestros clientes.
+      </p>
+
+      <h2>¿Cómo elegir un telefono?</h2>
+      <div className="video-container">
+        <p>En este video te mostramos cómo elegir un teléfono adecuado para tus necesidades. Desde el procesador hasta la memoria RAM, te explicamos los aspectos clave a considerar al momento de comprar un smartphone.</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/QQSbqBvhv10?si=LrD47_dJMBwkI0uQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
+
+      <h2>¿Cómo elegir una laptop?</h2>
+      <div className="video-container">
+        <p>En este video te mostramos cómo elegir una laptop adecuada para tus necesidades. Desde el procesador hasta la memoria RAM, te explicamos los aspectos clave a considerar al momento de comprar una laptop.</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/2yVeZRTkw_Q?si=TopbihDb7P6GTPJ1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
+
+      <h2>¿Necesitas saber más fondo?</h2>
+      <div className="video-container">
+        <p>
+          En este video te explicamos de manera sencilla la diferencia entre hardware y software. Descubre cómo estos dos conceptos fundamentales trabajan juntos para que tu computadora funcione correctamente. Ideal para quienes quieren entender mejor el mundo de la tecnología.
+        </p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/rC2PFygrkSs?si=eIkv153Kh7HqXb_o" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      </div>
+
+      <div className="button-container">
+        <a href="https://wa.me/51974293850" target="_blank" style={{ textDecoration: 'none' }}>
+          <button>Escríbeme al whatsapp</button>
+        </a>
+      </div>
+
+      <footer>
+        <div className="footer-top">
+          <div className="info">
+            <h4>
+              CONTACTANOS: <br />
+              📧 contacto@servitec.com <br />
+              📧 soporte@servitec.com (para atención técnica) <br />
+              📧 ventas@servitec.com (para consultas de compra)
+            </h4>
+          </div>
+          <hr className="sepador-footer" />
+          <div className="info">
+            <h4>
+              Síguenos en: <br />
+              📘 Facebook: @Servitec <br />
+              📸 Instagram: @Servitec <br />
+            </h4>
+          </div>
+          <hr className="sepador-footer" />
+          <div className="info info-rigth">
+            <h4>
+              📞 Teléfono: +51 987 654 321  <br />
+              🕒 Horario: Lunes - Sábado, 9:00 AM - 6:00 PM <br />
+              <a href="./../pages/mapSite.html">MAPA DE SITIO</a> <br />
+            </h4>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <h3>SERVITEC</h3>
+        </div>
+      </footer>
+    </div>
   )
 }
 
--- src/pages/servicios/Servicios.jsx
+++ src/pages/servicios/Servicios.jsx
@@ -2,7 +2,82 @@ import React from 'react'
 
 const Servicios = () => {
   return (
-    <div>Servicios</div>
+    <div>
+      <h1>Nuestros servicios</h1>
+      <h1 style={{
+        backgroundColor: 'rgba(109, 35, 35, 1)',
+        width: '250px',
+        textAlign: 'center',
+        fontSize: '250%',
+        padding: '30px 70px',
+        color: 'white',
+        borderRadius: '5px',
+        marginTop: '100px'
+      }}>
+        Reparacion y mantenimiento
+      </h1>
+      <h1 style={{ textAlign: 'center', marginTop: '100px' }}>
+        ¡No te preocupes! En ServiTec tenemos la solución. Reparamos computadoras, laptops, celulares y más.
+        No dejes que un fallo te detenga. ¡Visítanos hoy y deja tu equipo en manos de expertos!
+      </h1>
+      <div className="DefinicionServicio">
+        <img style={{ padding: '0px 20px' }} src="/img/servicios.png" alt="" />
+        <h2>
+          🛠️ Realizamos reparación rápida, 🔄
+          mantenimiento preventivo y 🚀 optimización de rendimiento.
+          Confía en nuestros expertos para que tu equipo vuelva a funcionar al 100%. 💻✅
+        </h2>
+      </div>
+      <a href="/pages/nuestros_locales.html" className="boton">UBICANOS</a>
+      <hr style={{ width: '1100px', backgroundColor: 'black', height: '4px', marginTop: '80px' }} />
+
+      <h1 style={{
+        backgroundColor: 'rgba(109, 35, 35, 1)',
+        width: '250px',
+        textAlign: 'center',
+        fontSize: '250%',
+        padding: '30px 70px',
+        color: 'white',
+        borderRadius: '5px',
+        marginTop: '100px'
+      }}>
+        Soporte tecnico.
+      </h1>
+      <h1 style={{ textAlign: 'center', marginTop: '100px' }}>
+        ¡No te preocupes! En ServiTec tenemos la solución. Reparamos computadoras, laptops, celulares y más.
+        No dejes que un fallo te detenga. ¡Visítanos hoy y deja tu equipo en manos de expertos!
+      </h1>
+      <div className="DefinicionServicio">
+        <img style={{ padding: '0px 20px' }} src="/img/ServicioTecnico.png" alt="" />
+        <h2>
+          🛠️ Realizamos reparación rápida, 🔄
+          mantenimiento preventivo y 🚀 optimización de rendimiento.
+          Confía en nuestros expertos para que tu equipo vuelva a funcionar al 100%. 💻✅
+        </h2>
+      </div>
+      <h1 style={{ textAlign: 'center', fontSize: '280%' }}>
+        Agenda tu cita
+      </h1>
+      <div className="form1">
+        <form action="" id="formulario">
+          <label style={{ fontSize: 'x-large', marginLeft: '6px' }}>Nombres</label>
+          <input type="text" /><br /><br />
+          <label style={{ fontSize: 'x-large' }}>Apellidos</label>
+          <input type="text" /><br /><br />
+          <label style={{ fontSize: 'x-large', marginLeft: '35px' }}>Email</label>
+          <input type="text" /><br /><br />
+          <label style={{ fontSize: 'x-large', marginLeft: '35px' }}>Fecha</label>
+          <input type="date" /><br /><br />
+          <input type="submit" style={{ marginLeft: '219px' }} />
+        </form>
+        <div id="fondoPantalla"></div>
+        <div id="estiloDeAlerta">
+          <p>Cita agendada con éxito</p>
+          <button id="cerrarAlert">Cerrar</button>
+        </div>
+      </div>
+      <div>Servicios</div>
+    </div>
   )
 }
 
