1c6901f2a772da3545641baf48ab3efe0178abb7
GalacticLang <jhedan0101@gmail.com>
2025-08-23 17:26:48 -0500

    footer duplicado

--- src/pages/negocio/Negocio.jsx
+++ src/pages/negocio/Negocio.jsx
@@ -42,40 +42,8 @@ const Negocio = () => {
           <button aria-label="Escríbeme al whatsapp">Escríbeme al whatsapp</button>
         </a>
       </div>
-
-      <footer>
-        <div className="footer-top">
-          <div className="info">
-            <h4>
-              CONTACTANOS: <br />
-              📧 contacto@servitec.com <br />
-              📧 soporte@servitec.com (para atención técnica) <br />
-              📧 ventas@servitec.com (para consultas de compra)
-            </h4>
-          </div>
-          <hr className="sepador-footer" />
-          <div className="info">
-            <h4>
-              Síguenos en: <br />
-              📘 Facebook: @Servitec <br />
-              📸 Instagram: @Servitec <br />
-            </h4>
-          </div>
-          <hr className="sepador-footer" />
-          <div className="info info-rigth">
-            <h4>
-              📞 Teléfono: +51 987 654 321  <br />
-              🕒 Horario: Lunes - Sábado, 9:00 AM - 6:00 PM <br />
-              <Link to="/mapSite">MAPA DE SITIO</Link> <br />
-            </h4>
-          </div>
-        </div>
-        <div className="footer-bottom">
-          <h3>SERVITEC</h3>
-        </div>
-      </footer>
     </div>
   )
 }
 
-export default Negocio
+export default
